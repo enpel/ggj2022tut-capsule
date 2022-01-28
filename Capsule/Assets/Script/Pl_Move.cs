311720b71dcee73cf16f81b7a29868b678b6cca7
Mitsuru_Watanabe <m011932572@edu.teu.ac.jp>
2022-01-28 23:12:53 +0900

    First time push test

---
+++ Capsule/Assets/Script/Pl_Move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pl_Move : MonoBehaviour
+{
+    //FirstTest
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector3(-1, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(1, 0, 0));
+        }
+    }
+}
