9cb09d055f5ba1c29ab3e381ad9a2057f227dd45
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-01-29 08:39:43 +0900

    ボタンクリックしたとき用のスクリプトを作りました

--- Capsule/Assets/Scripts/ButtonClick.cs
+++ Capsule/Assets/Scripts/ButtonClick.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStart : MonoBehaviour
+public class ButtonClick : MonoBehaviour
 {
-   // private int selectnum;
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
-       // if(Input.GetKeyDown(KeyCode.))
+        
     }
-    
 }
