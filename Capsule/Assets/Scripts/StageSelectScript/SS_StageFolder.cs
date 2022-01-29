a0530e2871aae88d323bcc17ab86b7a3dbc9318b
Mitsuru_Watanabe <m011932572@edu.teu.ac.jp>
2022-01-29 17:25:33 +0900

    ステージ選択画面、大方完成

--- Capsule/Assets/Scripts/StageSelectScript/SS_StageFolder.cs
+++ Capsule/Assets/Scripts/StageSelectScript/SS_StageFolder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 using ScriptableObject;
 
 public class SS_StageFolder : MonoBehaviour
@@ -18,35 +19,43 @@ public class SS_StageFolder : MonoBehaviour
     public GameObject SLPrefab;
     private GameObject[] SSSGO;
     public GameObject SSSPrefab;
+    private GameObject[] ASGO;
+    public GameObject ASPrefab;
 
     public GameObject StageList;
     private RectTransform[] SLRT;
     public GameObject SSS;
     private RectTransform[] SSSRT;
     public GameObject AS;
-    private RectTransform ASRT;
+    private RectTransform[] ASRT;
 
     void Start()
     {
         SLGO = new GameObject[SD.Length];
-        SSSGO = new GameObject[SD.Length];
         SLRT = new RectTransform[SD.Length];
+        SSSGO = new GameObject[SD.Length];
         SSSRT = new RectTransform[SD.Length];
+        ASGO = new GameObject[SD.Length];
+        ASRT = new RectTransform[SD.Length];
         for (int ii = 0; ii < SD.Length; ii++)
         {
             SLGO[ii] = Instantiate(SLPrefab, StageList.transform.position, StageList.transform.rotation, StageList.transform);
             SSSGO[ii] = Instantiate(SSSPrefab, SSS.transform.position, SSS.transform.rotation, SSS.transform);
+            ASGO[ii] = Instantiate(ASPrefab, AS.transform.position, AS.transform.rotation, AS.transform);
 
             SLGO[ii].GetComponent<Image>().sprite = Sprite.Create(SD[ii].StageThumbnail, new Rect(0, 0, SD[ii].StageThumbnail.width, SD[ii].StageThumbnail.height), Vector2.zero);
             SSSGO[ii].GetComponent<Image>().sprite = Sprite.Create(SD[ii].StageThumbnail, new Rect(0, 0, SD[ii].StageThumbnail.width, SD[ii].StageThumbnail.height), Vector2.zero);
+            ASGO[ii].GetComponent<TextMeshProUGUI>().text = MakeStageOverview(ii, $"<color=#{0xBBBB00FF:X}>WriteStageState</color>");
+            // WriteStageState = Clear!! toka NewStage toka
 
             SLRT[ii] = SLGO[ii].GetComponent<RectTransform>();
             SSSRT[ii] = SSSGO[ii].GetComponent<RectTransform>();
+            ASRT[ii] = ASGO[ii].GetComponent<RectTransform>();
             float mn = -ii + StageNumNow + -(StageNumNow - StageNumBefore) / Mathf.Abs(StageNumNow - StageNumBefore) * MoveTimeNow / MoveTimeLong;
             SLRT[ii].anchoredPosition = new Vector3(mn * -530, SLRT[ii].anchoredPosition.y, 0);
             SSSRT[ii].anchoredPosition = new Vector3(mn * -1170, SSSRT[ii].anchoredPosition.y, 0);
+            ASRT[ii].anchoredPosition = new Vector3(mn * -700, ASRT[ii].anchoredPosition.y, 0);
         }
-        ASRT = AS.GetComponent<RectTransform>();
 
         StageNumBefore = StageNumNow;
         StageNumLimit = SD.Length;
@@ -64,6 +73,27 @@ public class SS_StageFolder : MonoBehaviour
             MoveTimeNow = 0;
             StageListMove();
         }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StageNumDec();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StageNumInc();
+        }
+    }
+
+    public string MakeStageOverview(int folderNum, string StageStateText)
+    {
+
+        // 6GyoumeMade
+        return "No. " + (folderNum + 1)
+            + "\n" + SD[folderNum].StageName
+            + "\n" + "Creator:" + SD[folderNum].LevelDesigner.Name
+            + "\n" + ""
+            + "\n" + StageStateText
+            + "\n" + "";
     }
 
     public void StageNumInc()
@@ -114,9 +144,8 @@ public class SS_StageFolder : MonoBehaviour
                 float mn = -ii + StageNumNow + -(StageNumNow - StageNumBefore) / Mathf.Abs(StageNumNow - StageNumBefore) * MoveTimeNow / MoveTimeLong;
                 SLRT[ii].anchoredPosition = new Vector3(mn * -530, SLRT[ii].anchoredPosition.y, 0);
                 SSSRT[ii].anchoredPosition = new Vector3(mn * -1170, SSSRT[ii].anchoredPosition.y, 0);
+                ASRT[ii].anchoredPosition = new Vector3(mn * -700, ASRT[ii].anchoredPosition.y, 0);
             }
-            float mnAS = StageNumNow + -(StageNumNow - StageNumBefore) / Mathf.Abs(StageNumNow - StageNumBefore) * MoveTimeNow / MoveTimeLong;
-            ASRT.anchoredPosition = new Vector3(mnAS * -700, ASRT.anchoredPosition.y, 0);
         }
     }
 
