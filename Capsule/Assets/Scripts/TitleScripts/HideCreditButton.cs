70793db51c835bcbee95bd7f26513966248ebb0b
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-01-29 14:59:05 +0900

    設定ボタンの追加と設定の表示を作りました

--- Capsule/Assets/Scripts/TitleScripts/HideCreditButton.cs
+++ Capsule/Assets/Scripts/TitleScripts/HideCreditButton.cs
@@ -7,10 +7,12 @@ public class HideCreditButton : MonoBehaviour
     [SerializeField] GameObject Credit;
     [SerializeField] GameObject Creditbutton;
     [SerializeField] GameObject HideCreditButtonUI;
+    [SerializeField] GameObject OptionButton;
     public void ClickButton()
     {
         Credit.SetActive(false);
         Creditbutton.SetActive(true);
         HideCreditButtonUI.SetActive(false);
+        OptionButton.SetActive(true);
     }
 }
