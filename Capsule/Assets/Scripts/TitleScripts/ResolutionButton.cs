63972bc1bb6b48ace3b7e713059c74fec694e8dd
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-01-29 16:20:46 +0900

    タイトルのキャンバスの画面対応を変更しました

---
+++ Capsule/Assets/Scripts/TitleScripts/ResolutionButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionButton : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.SetResolution(640, 480, true);
+    }
+    void Update()
+    {
+        
+    }
+    public void ClickButton()
+    {
+        // Screen.SetResolution(1280, 720, true);
+        Screen.SetResolution(320, 240, false);
+    }
+}
