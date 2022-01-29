d6b3cee37146d82a810db10cf0f0a9efe343e3b2
enpel <enpel715@gmail.com>
2022-01-29 13:50:54 +0900

    Add StageData and LevelDesignerData

---
+++ Capsule/Assets/Scripts/ScriptableObject/LevelDesignerData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ScriptableObject
+{
+    [CreateAssetMenu(fileName = "LevelDesigner", menuName = "ゲームデータ/レベルデザイナーデータ", order = 2)]
+    public class LevelDesignerData : UnityEngine.ScriptableObject
+    {
+        [SerializeField] private string 名前;
+        [SerializeField] private Texture2D アイコン;
+
+        public string Name => 名前;
+        public Texture2D IconTexture => アイコン;
+    }
+}
