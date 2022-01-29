8c16315881158a564a682bdac3402d852458dd63
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-01-29 10:37:46 +0900

    リトライボタンのスクリプトを入れました

--- Capsule/Assets/Scripts/ResultScripts/RetryButton.cs
+++ Capsule/Assets/Scripts/ResultScripts/RetryButton.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RetryButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ClickButton()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("DummyPlay");
     }
 }
