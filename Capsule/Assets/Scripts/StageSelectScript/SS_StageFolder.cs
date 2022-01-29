6aa2a5e2880cc4140daa69d5a63a0c96a5826506
Mitsuru_Watanabe <m011932572@edu.teu.ac.jp>
2022-01-29 16:20:17 +0900

    ステージ概要(左上)以外のステージ選択画面

--- Capsule/Assets/Scripts/StageSelectScript/SS_StageFolder.cs
+++ Capsule/Assets/Scripts/StageSelectScript/SS_StageFolder.cs
@@ -1,54 +1,127 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using ScriptableObject;
 
 public class SS_StageFolder : MonoBehaviour
 {
-    public int StageNumNow = 1;
-    public int StageNumLimit = 3;
+    private int StageNumNow = 0;
+    private int StageNumLimit = 3;
+    private int StageNumBefore = 1;
+    private float MoveTimeNow = 0;
+    public float MoveTimeLong = 1.0f;
+
+    public StageData[] SD;
+    private GameObject[] SLGO;
+    public GameObject SLPrefab;
+    private GameObject[] SSSGO;
+    public GameObject SSSPrefab;
 
     public GameObject StageList;
-    private RectTransform SLRT;
+    private RectTransform[] SLRT;
     public GameObject SSS;
-    private RectTransform SSSRT;
+    private RectTransform[] SSSRT;
     public GameObject AS;
     private RectTransform ASRT;
 
     void Start()
     {
-        SLRT = StageList.GetComponent<RectTransform>();
-        SSSRT = SSS.GetComponent<RectTransform>();
+        SLGO = new GameObject[SD.Length];
+        SSSGO = new GameObject[SD.Length];
+        SLRT = new RectTransform[SD.Length];
+        SSSRT = new RectTransform[SD.Length];
+        for (int ii = 0; ii < SD.Length; ii++)
+        {
+            SLGO[ii] = Instantiate(SLPrefab, StageList.transform.position, StageList.transform.rotation, StageList.transform);
+            SSSGO[ii] = Instantiate(SSSPrefab, SSS.transform.position, SSS.transform.rotation, SSS.transform);
+
+            SLGO[ii].GetComponent<Image>().sprite = Sprite.Create(SD[ii].StageThumbnail, new Rect(0, 0, SD[ii].StageThumbnail.width, SD[ii].StageThumbnail.height), Vector2.zero);
+            SSSGO[ii].GetComponent<Image>().sprite = Sprite.Create(SD[ii].StageThumbnail, new Rect(0, 0, SD[ii].StageThumbnail.width, SD[ii].StageThumbnail.height), Vector2.zero);
+
+            SLRT[ii] = SLGO[ii].GetComponent<RectTransform>();
+            SSSRT[ii] = SSSGO[ii].GetComponent<RectTransform>();
+            float mn = -ii + StageNumNow + -(StageNumNow - StageNumBefore) / Mathf.Abs(StageNumNow - StageNumBefore) * MoveTimeNow / MoveTimeLong;
+            SLRT[ii].anchoredPosition = new Vector3(mn * -530, SLRT[ii].anchoredPosition.y, 0);
+            SSSRT[ii].anchoredPosition = new Vector3(mn * -1170, SSSRT[ii].anchoredPosition.y, 0);
+        }
         ASRT = AS.GetComponent<RectTransform>();
+
+        StageNumBefore = StageNumNow;
+        StageNumLimit = SD.Length;
     }
 
     void Update()
     {
-
+        if (MoveTimeNow > 0)
+        {
+            MoveTimeNow -= Time.deltaTime;
+            StageListMove();
+        }
+        if (MoveTimeNow < 0)
+        {
+            MoveTimeNow = 0;
+            StageListMove();
+        }
     }
 
     public void StageNumInc()
     {
-        if (StageNumNow < StageNumLimit)
-            StageNumNow++;
-        else
-            StageNumNow = StageNumLimit;
+        if (MoveTimeNow <= 0)
+        {
+            MoveTimeNow = MoveTimeLong;
+            StageNumBefore = StageNumNow;
 
-        StageListMove();
+            if (StageNumNow < StageNumLimit - 1)
+            {
+                StageNumNow++;
+            }
+            else
+            {
+                StageNumNow = StageNumLimit - 1;
+            }
+
+            StageListMove();
+        }
     }
     public void StageNumDec()
     {
-        if (StageNumNow > 1)
-            StageNumNow--;
-        else
-            StageNumNow = 1;
+        if (MoveTimeNow <= 0)
+        {
+            MoveTimeNow = MoveTimeLong;
+            StageNumBefore = StageNumNow;
 
-        StageListMove();
+            if (StageNumNow > 0)
+            {
+                StageNumNow--;
+            }
+            else
+            {
+                StageNumNow = 0;
+            }
+
+            StageListMove();
+        }
     }
 
     public void StageListMove()
     {
-        SLRT.anchoredPosition = new Vector3((StageNumNow - 1) * -530, SLRT.anchoredPosition.y, 0);
-        SSSRT.anchoredPosition = new Vector3((StageNumNow - 1) * -1170, SLRT.anchoredPosition.y, 0);
-        ASRT.anchoredPosition = new Vector3((StageNumNow - 1) * -700, SLRT.anchoredPosition.y, 0);
+        if (StageNumNow != StageNumBefore)
+        {
+            for (int ii = 0; ii < SD.Length; ii++)
+            {
+                float mn = -ii + StageNumNow + -(StageNumNow - StageNumBefore) / Mathf.Abs(StageNumNow - StageNumBefore) * MoveTimeNow / MoveTimeLong;
+                SLRT[ii].anchoredPosition = new Vector3(mn * -530, SLRT[ii].anchoredPosition.y, 0);
+                SSSRT[ii].anchoredPosition = new Vector3(mn * -1170, SSSRT[ii].anchoredPosition.y, 0);
+            }
+            float mnAS = StageNumNow + -(StageNumNow - StageNumBefore) / Mathf.Abs(StageNumNow - StageNumBefore) * MoveTimeNow / MoveTimeLong;
+            ASRT.anchoredPosition = new Vector3(mnAS * -700, ASRT.anchoredPosition.y, 0);
+        }
+    }
+
+    public void StartStage()
+    {
+        SceneManager.LoadScene(SD[StageNumNow].StageScene);
     }
 }
