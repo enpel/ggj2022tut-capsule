e6a257ec68429586ccc29080079be005369e47fb
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-01-29 13:08:12 +0900

    ゴールの機能を追加したDummySceneを作りました

--- Capsule/Assets/Scripts/DummyPlay/GoalScript.cs
+++ Capsule/Assets/Scripts/DummyPlay/GoalScript.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Result");
+        }
     }
 }
