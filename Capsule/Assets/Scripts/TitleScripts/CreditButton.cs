ce14087cae714bc590ab34306a1d4ea3aa47c2f1
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-01-29 11:40:50 +0900

    クレジットを表示するボタンの位置を、クレジット表示時にクレジットボタンを非表示にするボタンに変更しました

--- Capsule/Assets/Scripts/TitleScripts/CreditButton.cs
+++ Capsule/Assets/Scripts/TitleScripts/CreditButton.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class CreditButton : MonoBehaviour
 {
     [SerializeField] GameObject Credit;
+    [SerializeField] GameObject Creditbutton;
     [SerializeField] GameObject HideCreditButton;
     public void ClickButton()
     {
         Credit.SetActive(true);
+        Creditbutton.SetActive(false);
         HideCreditButton.SetActive(true);
     }
 }
--- Capsule/Assets/Scripts/TitleScripts/HideCreditButton.cs
+++ Capsule/Assets/Scripts/TitleScripts/HideCreditButton.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class HideCreditButton : MonoBehaviour
 {
     [SerializeField] GameObject Credit;
+    [SerializeField] GameObject Creditbutton;
     [SerializeField] GameObject HideCreditButtonUI;
     public void ClickButton()
     {
         Credit.SetActive(false);
+        Creditbutton.SetActive(true);
         HideCreditButtonUI.SetActive(false);
     }
 }
