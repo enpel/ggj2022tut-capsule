ecd609495534d826122af1e99694c7aa7a266ed8
Mitsuru_Watanabe <m011932572@edu.teu.ac.jp>
2022-01-30 10:08:25 +0900

    SEを一つ追加

--- Capsule/Assets/Scripts/DummyPlay/Miss.cs
+++ Capsule/Assets/Scripts/DummyPlay/Miss.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Core.Global;
+using Sound;
 
 public class Miss : MonoBehaviour
 {
+    static 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            Global.SoundPlayer.PlaySE(SeType.GariPunch);
             SceneManager.LoadScene("PreInGame");
         }
     }
